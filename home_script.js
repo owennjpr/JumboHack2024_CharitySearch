2d3e02019370c37b4ab23b3b0d361026924b5b95
Owen Prendergast <owenprendergast8@gmail.com>
2024-02-18 13:21:06 -0500

    added expanding descriptions

--- home_script.js
+++ home_script.js
@@ -184,6 +184,93 @@ function searchTag(tag) {
 	// 		.catch (error => console.log(error))
 }
 
+function displayDescriptionLong(orgID) {
+    console.log("printing long description for " + orgID);
+    const toReplace = document.getElementById(orgID);
+    let outText = "";
+    res = fetch("https://partners.every.org/v0.2/nonprofit/" + orgID + "?apiKey=pk_live_3fcf5c20985c68d7907fc33ea5ef9778")
+        .then (res => res.text())
+        .then (data =>
+        {
+            console.log(data);
+            data = JSON.parse(data);
+            let longDescription = data.data.nonprofit.descriptionLong;
+            if (longDescription === null) {
+                outText = "No extended description provided by this organization."
+            } else {
+                outText = longDescription;
+            }
+
+            if (data.data.nonprofit.websiteUrl == null) {
+                url = data.data.nonprofit.profileUrl;
+            } else {
+                url = data.data.nonprofit.websiteUrl;
+            }
+            let elmUrl = "";
+            if (data.data.nonprofit.logoUrl !== undefined) {
+                elmUrl = data.data.nonprofit.logoUrl;
+            } else {
+                elmUrl = "blankLogo.png"
+            }
+
+            let inner = `<div class="org-list" id="${orgID}"> <img class="orgPic" src="${elmUrl}" alt="orgPic" class="orgPic">
+
+             <a href="${url}">${data.data.nonprofit.name}</a>
+            </br>
+            <h8>
+            ${outText}
+            </h8>
+            <button class="btn filters" id="btn${orgID}" onclick=revertDescriptionLong("${orgID}")>read less</button>
+            </div>`  
+        
+            toReplace.innerHTML = inner;
+        })
+}
+
+function revertDescriptionLong(orgID) {
+    console.log("reverting long description for " + orgID);
+    const toReplace = document.getElementById(orgID);
+    let outText = "";
+    res = fetch("https://partners.every.org/v0.2/nonprofit/" + orgID + "?apiKey=pk_live_3fcf5c20985c68d7907fc33ea5ef9778")
+        .then (res => res.text())
+        .then (data =>
+        {
+            console.log(data);
+            data = JSON.parse(data);
+            let description = data.data.nonprofit.description;
+            if (description === null) {
+                outText = "No description provided by this organization."
+            } else {
+                outText = description;
+            }
+
+            if (data.data.nonprofit.websiteUrl == null) {
+                url = data.data.nonprofit.profileUrl;
+            } else {
+                url = data.data.nonprofit.websiteUrl;
+            }
+            let elmUrl = "";
+            if (data.data.nonprofit.logoUrl !== undefined) {
+                elmUrl = data.data.nonprofit.logoUrl;
+            } else {
+                elmUrl = "blankLogo.png"
+            }
+
+            let inner = `<div class="org-list" id="${orgID}"> <img class="orgPic" src="${elmUrl}" alt="orgPic" class="orgPic">
+
+             <a href="${url}">${data.data.nonprofit.name}</a>
+            </br>
+            <h8>
+            ${outText}
+            </h8>
+            <button class="btn filters" id="btn${orgID}" onclick=displayDescriptionLong("${orgID}")>read more</button>
+            </div>`  
+        
+            toReplace.innerHTML = inner;
+        })
+}
+
+
 function displayOrg(elm) {
     let description = "";
     if (!(elm.ein === undefined)) {
@@ -208,14 +295,16 @@ function displayOrg(elm) {
                 } else {
                     elmUrl = "blankLogo.png"
                 }
-                console.log(url + "     EIN");
-                let inner = `<div class="org-list"> <img class="orgPic" src="${elmUrl}" alt="orgPic" class="orgPic">
+                console.log(elm.name);
+                let temp1 = "HIII 1";
+                let inner = `<div class="org-list" id="${elm.ein}"> <img class="orgPic" src="${elmUrl}" alt="orgPic" class="orgPic">
 
                 <a href="${url}">${elm.name}</a>
                 </br>
                 <h8>
                     ${description}
                 </h8>
+                <button class="btn filters" id="btn${elm.ein}" onclick=displayDescriptionLong("${elm.ein}")>read more</button>
                 </div>
                 <hr>`  
                 
@@ -247,14 +336,16 @@ function displayOrg(elm) {
                 } else {
                     elmUrl = "blankLogo.png"
                 }
-                console.log(url);
-                let inner = `<div class="org-list"> <img class="orgPic" src="${elmUrl}" alt="orgPic" class="orgPic">
+                console.log(elm.name);
+                let temp1 = "HIII 2";
+                let inner = `<div class="org-list" id="${elm.slug}"> <img class="orgPic" src="${elmUrl}" alt="orgPic" class="orgPic">
                 <a href="${url}">${elm.name}</a>
 
                 </br>
                 <h8>
                     ${description}
                 </h8>
+                <button class="btn filters" id="btn${elm.slug}" onclick=displayDescriptionLong("${elm.slug}")>read more</button>
                 </div>
                 <hr>`
 
@@ -265,3 +356,6 @@ function displayOrg(elm) {
     }
     
 }
+
+
+
